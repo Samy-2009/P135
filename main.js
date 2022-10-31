b35724d5b16afb6c97b66c330ffb4d018844774b
Samy-2009 <97361048+Samy-2009@users.noreply.github.com>
2022-10-31 12:15:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+objects = [];
+status = "";
+
 function setup(){
     canvas = createCanvas(300,200);
     canvas.center();
@@ -19,4 +22,34 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 300, 200);
+    if(status !=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Estado: Objeto Detectado";
+
+            fill("#FF0000");
+                percent = floor(objects[i].confidence*100);
+                text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if (objects [i].label == nobjeto){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("status").innerHTML = "Se detecto el "+ nobjeto;
+            }else{
+                document.getElementById("status").innerHTML = "No se detecto el " + nobjeto;
+            }
+        }
+
+    }
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
 }
